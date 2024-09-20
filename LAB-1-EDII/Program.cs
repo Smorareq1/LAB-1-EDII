cc40283a825008add874642380c7c1a94ff7afa9
Smorareq1 <smorareq1@gmail.com>
2024-09-20 14:26:43 -0600

    Aprobado el de 200k 5mins

--- LAB-1-EDII/Program.cs
+++ LAB-1-EDII/Program.cs
@@ -12,11 +12,13 @@ class Program
             string archivoResultados = "C:\\Users\\smora\\Downloads\\resultados_busquedas.txt";
 
             // C:\Users\smora\Downloads\100Klab01\100Klab01_books.csv - C:\Users\smora\Downloads\100Klab01\100Klab01_search.csv
+            // C:\Users\smora\Downloads\200Klab01\200Klab01_books.csv - C:\Users\smora\Downloads\200Klab01\200Klab01_search.csv
+            
             
             Console.WriteLine("Ingrese la ruta del archivo: ");
-            string filepath = "C:\\Users\\smora\\Downloads\\100Klab01\\100Klab01_books.csv";
+            string filepath = "C:\\Users\\smora\\Downloads\\200Klab01\\200Klab01_books.csv";
             Console.WriteLine("Ingrese el archivo de busqueda: ");
-            string searchFile = "C:\\Users\\smora\\Downloads\\100Klab01\\100Klab01_search.csv";
+            string searchFile = "C:\\Users\\smora\\Downloads\\200Klab01\\200Klab01_search.csv";
 
             // Verificar si el archivo existe
             if (!System.IO.File.Exists(filepath) || !System.IO.File.Exists(searchFile))
@@ -26,6 +28,7 @@ class Program
             }
 
             gestorPrincipal.ProcesarArchivoInsertar(filepath);
+            Console.WriteLine("Procesando archivo de busqueda");
             gestorPrincipal.ProcesarArchivoBusqueda(searchFile, archivoResultados);
             Console.WriteLine("Archivo de salida genrado en descargas con el nombre de resultados_busquedas.txt");
             
