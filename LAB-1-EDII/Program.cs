a03892b79697de92254267c9198aac53a4bef62e
Smorareq1 <smorareq1@gmail.com>
2024-09-20 14:44:19 -0600

    FINAL

--- LAB-1-EDII/Program.cs
+++ LAB-1-EDII/Program.cs
@@ -6,19 +6,17 @@ class Program
     {
         try
         {
+            // C:\Users\smora\Downloads\100Klab01\100Klab01_books.csv - C:\Users\smora\Downloads\100Klab01\100Klab01_search.csv
+            
             ArbolB tree = new ArbolB(30);
             GestorDeArchivos gestorPrincipal = new GestorDeArchivos(tree);
             
             string archivoResultados = "C:\\Users\\smora\\Downloads\\resultados_busquedas.txt";
-
-            // C:\Users\smora\Downloads\100Klab01\100Klab01_books.csv - C:\Users\smora\Downloads\100Klab01\100Klab01_search.csv
-            // C:\Users\smora\Downloads\200Klab01\200Klab01_books.csv - C:\Users\smora\Downloads\200Klab01\200Klab01_search.csv
-            
             
             Console.WriteLine("Ingrese la ruta del archivo: ");
-            string filepath = "C:\\Users\\smora\\Downloads\\200Klab01\\200Klab01_books.csv";
+            string filepath = Console.ReadLine();
             Console.WriteLine("Ingrese el archivo de busqueda: ");
-            string searchFile = "C:\\Users\\smora\\Downloads\\200Klab01\\200Klab01_search.csv";
+            string searchFile = Console.ReadLine();
 
             // Verificar si el archivo existe
             if (!System.IO.File.Exists(filepath) || !System.IO.File.Exists(searchFile))
